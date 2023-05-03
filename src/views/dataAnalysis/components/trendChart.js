b6560f040840abe84060c6dec93c1cd613bb834e
WengranXiao <112583498+WengranXiao@users.noreply.github.com>
2023-05-03 19:35:41 -0400

    Update trendChart.js

--- src/views/dataAnalysis/components/trendChart.js
+++ src/views/dataAnalysis/components/trendChart.js
@@ -1,35 +1,77 @@
-import React, { Component } from 'react';
-import { Chart } from '@antv/g2';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { Line } from '@ant-design/plots';
 
 
-class TrendChart extends Component {
-  componentDidMount() {
-    var chart = new Chart({
-      container: document.getElementById('TrendChart'),
-      animate: false,
-      height: 400,
-      theme: 'classic',
-    });
-    chart
-      .interval()
-      .data([
-        { genre: 'Sports', sold: 275 },
-        { genre: 'Strategy', sold: 115 },
-        { genre: 'Action', sold: 120 },
-        { genre: 'Shooter', sold: 350 },
-        { genre: 'Other', sold: 150 },
-      ])
-      .encode('x', 'genre')
-      .encode('y', 'sold')
-      .encode('color', 'genre');
+const TrendChart = () => {
+  
+  const data=[
+    {
+      year:2018,
+      value:1.2,
+      category: '产品线A'
+    },
+    {
+      year:2019,
+      value:1.1,
+      category: '产品线A'
+    },
+    {
+      year:2018,
+      value:1.1,
+      category: '产品线B'
+    },
+    {
+      year:2019,
+      value:1.0,
+      category: '产品线B'
+    },
+    {
+      year:2020,
+      value: 2,
+      category: '产品线A'
+    },
+    {
+      year:2020,
+      value:1.9,
+      category: '产品线B'
+    },
+    {
+      year:2021,
+      value:5,
+      category: '产品线A'
+    },
+    {
+      year:2021,
+      value:4.8,
+      category: '产品线B'
+    },
+    {
+      year:2022,
+      value:1.0,
+      category: '产品线A'
+    },
+    {
+      year:2022,
+      value:1.5,
+      category: '产品线B'
+    },
+  ]
+  const config = {
+    data: data.reverse(),
+    xField: 'year',
+    yField: 'value',
+    seriesField: 'category',
+    yAxis: {
+      label: {
+        // 数值格式化为千分位
+        formatter: (v) => `${v}`.replace(/\d{1,3}(?=(\d{3})+$)/g, (s) => `${s},`),
+      },
+    },
+  
+  };
 
-    chart.render();
-  }
-  render() {
-    return (
-      <div id="TrendChart" />
-    );
-  }
-}
+  return <Line {...config} />;
+};
 
 export default TrendChart;
